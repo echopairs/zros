a445af5ad8fbc35e424f607a56efd8015c6d7433
zhengshengyang <zhengshengyang@njrobot.com>
2016-10-24 21:35:28 -0700

    update gen_grpc.sh for supports centos && ubuntu

--- tools/gen_grpc.sh
+++ tools/gen_grpc.sh
@@ -8,21 +8,60 @@
 
 script=$(readlink -f "$0")
 route=$(dirname "$script")
+
 HAS_CHECKINSTALL=`which checkinstall >/dev/null 2>&1 && echo true || echo false`
-echo $HAS_CHECKINSTALL
-if [ "$HAS_CHECKINSTALL"x=="false"x ]; then 
+
+UBUNTU_FLAG=`cat /etc/issue | grep -i ubuntu >/dev/null 2>&1 && echo true || echo false`
+CENTOS_FLAG=`cat /etc/issue | grep -i centos >/dev/null 2>&1 && echo true || echo false`
+
+if [ "$HAS_CHECKINSTALL"x = "false"x ]; then 
 echo "--please install checkinstall first"
 echo "-- down src : git clone http://checkinstall.izto.org/checkinstall.git"
 exit -1;
 fi
 
+# init autoconf 
+if [ "$UBUNTU_FLAG"X = "true"X ]; then
+echo "-- this is ubuntu system"
+echo "--begin install autoconf"
+sudo apt-get install build-essential autoconf libtool >/dev/null 2>&1
+else if [ "CENTOS_FLAG"x = "true"x ];then
+echo "-- this is centos system"
+echo "--begin install autoconf"
+yum -y install build-essential autoconf libtool >/dev/null 2>&1
+else
+echo "-- not supports system except centos and ubuntu"
+fi
+fi
+
+:<<!
+# get source code if dir exsits just remove it.(the code may be incomplete)
+[ -d "$route/grpc" ] && rm -rf grpc
+
+git clone -b $(curl -L http://grpc.io/release) https://github.com/grpc/grpc
+!
+# update submodule
+cd grpc
+git submodule update --init
+
 # init grpc
+make -j 2 || (echo "init grpc failed" exit)
+## checkinstall upon system
+if [ "$UBUNTU_FLAG"X = "true"X ]; then
+sudo checkinstall -D --install=no --fstrans=no --default --pkgname=grpc --pkgversion=1.0.0 --pakdir=$route || exit 2
+else
+sudo checkinstall -R --install=no --fstrans=no --default --pkgname=grpc --pkgversion=1.0.0 --pakdir=$route || exit 2
+fi
 
+# init protobuf 
 
-# init protobuf
+cd third_party/protobuf
+autoreconf -f -i -Wall,no-obsolete
+./configure
+make -j 2 || exit 3
 
-#git clone -b $(curl -L http://grpc.io/release) https://github.com/grpc/grpc
-#cd grpc
-#git submodule update --init
-#make
-#make install
+if [ "$UBUNTU_FLAG"X = "true"X ]; then
+sudo checkinstall -D --install=no --fstrans=no --default --pkgname=protobuf --pkgversion=3.0.0-GA --pakdir=$route || exit 4
+else
+sudo checkinstall -D --install=no --fstrans=no --default --pkgname=protobuf --pkgversion=3.0.0-GA --pakdir=$route || exit 4
+fi
