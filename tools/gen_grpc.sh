9b8a9154a1d2e10aab6681430c29436ae9914825
echopair <736418319@qq.com>
2016-10-26 19:53:59 -0400

    update gen_grpc.sh fix no rpmbuild

--- tools/gen_grpc.sh
+++ tools/gen_grpc.sh
@@ -25,7 +25,7 @@ if [ "$UBUNTU_FLAG"X = "true"X ]; then
 echo "-- this is ubuntu system"
 echo "--begin install autoconf"
 sudo apt-get install build-essential autoconf libtool >/dev/null 2>&1
-else if [ "CENTOS_FLAG"x = "true"x ];then
+else if [ "$CENTOS_FLAG"x = "true"x ];then
 echo "-- this is centos system"
 echo "--begin install autoconf"
 yum -y install build-essential autoconf libtool >/dev/null 2>&1
@@ -50,7 +50,12 @@ make -j 2 || (echo "init grpc failed" exit)
 if [ "$UBUNTU_FLAG"X = "true"X ]; then
 sudo checkinstall -D --install=no --fstrans=no --default --pkgname=grpc --pkgversion=1.0.0 --pakdir=$route || exit 2
 else
-sudo checkinstall -R --install=no --fstrans=no --default --pkgname=grpc --pkgversion=1.0.0 --pakdir=$route || exit 2
+echo "/usr/local/lib64" >/etc/ld.so.conf.d/installwatch.conf
+ldconfig
+ln -s /usr/local/lib/installwatch.so /usr/local/lib64/installwatch.so
+yum install -y rpm-build
+yum install -y gcc rpm-build pcre-devel rpmdevtools
+checkinstall -R --install=no --fstrans=no --default --pkgname=grpc --pkgversion=1.0.0 --pakdir=$route || exit 2
 fi
 
 # init protobuf 
@@ -63,5 +68,5 @@ make -j 2 || exit 3
 if [ "$UBUNTU_FLAG"X = "true"X ]; then
 sudo checkinstall -D --install=no --fstrans=no --default --pkgname=protobuf --pkgversion=3.0.0-GA --pakdir=$route || exit 4
 else
-sudo checkinstall -D --install=no --fstrans=no --default --pkgname=protobuf --pkgversion=3.0.0-GA --pakdir=$route || exit 4
+/usr/local/sbin/checkinstall -D --install=no --fstrans=no --default --pkgname=protobuf --pkgversion=3.0.0-GA --pakdir=$route || exit 4
 fi
