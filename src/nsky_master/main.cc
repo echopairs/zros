c8f8aa23376f68f3b898f48f57144b1a7eeb5f9b
sky_walker <zhengshengyang@njrobot.com>
2016-11-15 02:53:15 -0800

    add grpc interface function

--- src/nsky_master/main.cc
+++ src/nsky_master/main.cc
@@ -1,7 +1,24 @@
-// TODO zsy
-// main function
 #include <iostream>
-int main()
+#include <nsky/common/signal_handle.h>
+//#include <nsky/common/slog.h>
+
+#include "nsky_master.h"
+
+using namespace nsky;
+int main(int argc, char* argv[])
 {
-	// TODO
+    try
+    {
+        //slog::InitLog("nsky_master");
+        BaseServiceProcess base;
+        auto address = argc > 1 ? argv[1] : "0.0.0.0:23333";
+        //nsky::MasterServiceImpl master(address);
+        //master.RunServer();
+    }
+    catch (const std::exception &e)
+    {
+      //  NROS_LOG_ERROR << "Catch fatal exception: " << e.what();
+        throw;
+    }
+    return 0;
 }
--- src/nsky_master/nsky_master.h
+++ src/nsky_master/nsky_master.h
@@ -1,1 +1,33 @@
 // impl grpc interface
+#include <thread>
+#include <memory>
+#include <string>
+#include <thread>
+#include <chrono>
+
+#include <grpc++/grpc++.h>
+#include "nsky_rpc.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+namespace nsky {
+	class MasterServiceImpl final : public nsky_rpc::MasterRPC::Service
+	{
+	public:
+		MasterServiceImpl(std::string &address);
+		grpc::Status RegisterServer(::grpc::ServerContext* context, const ::nsky_rpc::ServerInfo* request, ::nsky_rpc::Status* response) override;
+		grpc::Status UnregisterServer(::grpc::ServerContext* context, const ::nsky_rpc::ServerInfo* request, ::nsky_rpc::Status* response) override;
+		grpc::Status RegisterClient(::grpc::ServerContext* context, const ::nsky_rpc::ClientInfo* request, ::nsky_rpc::Status* response) override;
+		grpc::Status UnregisterClient(::grpc::ServerContext* context, const ::nsky_rpc::ClientInfo* request, ::nsky_rpc::Status* response) override;
+		grpc::Status Ping(::grpc::ServerContext* context, const ::nsky_rpc::PingRequest* request, ::nsky_rpc::Status* response) override;
+		void RunServer();
+	private:
+		std::string server_address_;
+		grpc::ServerBuilder builder_;
+		std::unique_ptr<grpc::Server> server_;
+
+	};
+}
