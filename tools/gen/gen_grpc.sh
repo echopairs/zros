0bd1533f7be4ec716dca4105d38d71e311c07b47
echopairs <haozhengshengyang@163.com>
2018-07-30 04:26:43 -0700

    close #12

--- tools/gen/gen_grpc.sh
+++ tools/gen/gen_grpc.sh
@@ -9,34 +9,6 @@
 # env: ubuntu 16.4
 #-------------------------------------
 
-script=$(readlink -f "$0")
-route=$(dirname "$script")
-
-function check_package_is_exist() {
-    if [ -f ${route}/../../external_libs/debs/grpc* ]; then
-        echo "grpc deb already gen"
-        exit 0
-    fi
-
-    if [ -f ${route}/../../external_libs/rpms/grpc* ]; then
-        echo "grpc rpm already gen"
-        exit 0
-    fi
-}
-
-check_package_is_exist
-
-HAS_CHECKINSTALL=`which checkinstall >/dev/null 2>&1 && echo true || echo false`
-
-UBUNTU_FLAG=`cat /etc/issue | grep -i ubuntu >/dev/null 2>&1 && echo true || echo false`
-CENTOS_FLAG=`cat /etc/issue | grep -i centos >/dev/null 2>&1 && echo true || echo false`
-
-
-if [ "$HAS_CHECKINSTALL"x = "false"x ]; then
-echo "--please install checkinstall first"
-echo "-- down src : git clone http://checkinstall.izto.org/checkinstall.git"
-exit -1;
-fi
 
 # init autoconf
 if [ "$UBUNTU_FLAG"X = "true"X ]; then
@@ -54,9 +26,9 @@ fi
 
 
 # get source code if dir exsits just remove it.(the code may be incomplete)
-[ -d "$route/grpc" ] && rm -rf grpc
+[ -d "$route/grpc" ] && sudo rm -rf $route/grpc
 
-git clone -b $(curl -L http://grpc.io/release) https://github.com/grpc/grpc
+git clone -b $(curl -L http://grpc.io/release) https://github.com/grpc/grpc >/dev/null 2>&1
 
 # update submodule
 cd grpc
@@ -72,14 +44,14 @@ protobuf_version=`echo ${protobuf_branch##*' '}`
 make -j 2 || (echo "init grpc failed" exit)
 ## checkinstall upon system
 if [ "$UBUNTU_FLAG"X = "true"X ]; then
-    sudo checkinstall -D --install=no --fstrans=no --default --pkgname=grpc --pkgversion=${grpc_version} --pakdir=$route/../../external_libs/debs || exit 2
+    sudo checkinstall -D --install=no --fstrans=no --default --pkgname=grpc --pkgversion=${grpc_version} --pakdir=$route/../../external_libs/debs >/dev/null 2>&1 || exit 2
 else
     echo "/usr/local/lib64" >/etc/ld.so.conf.d/installwatch.conf
     ldconfig
     ln -s /usr/local/lib/installwatch.so /usr/local/lib64/installwatch.so
     yum install -y rpm-build
     yum install -y gcc rpm-build pcre-devel rpmdevtools
-    checkinstall -R --install=no --fstrans=no --default --pkgname=grpc --pkgversion=${grpc_version} --pakdir=$route/../../external_libs/rpms || exit 2
+    checkinstall -R --install=no --fstrans=no --default --pkgname=grpc --pkgversion=${grpc_version} --pakdir=$route/../../external_libs/rpms >/dev/null 2>&1 || exit 2
 fi
 
 # init protobuf
@@ -87,12 +59,14 @@ fi
 cd third_party/protobuf
 autoreconf -f -i -Wall,no-obsolete
 ./configure
-make -j 2 || exit 3
+make -j 2 >/dev/null 2>&1 || exit 3
 
 if [ "$UBUNTU_FLAG"X = "true"X ]; then
-    sudo checkinstall -D --install=no --fstrans=no --default --pkgname=protobuf --pkgversion=${protobuf_version} --pakdir=$route/../../external_libs/debs || exit 4
+    sudo checkinstall -D --install=no --fstrans=no --default --pkgname=protobuf --pkgversion=${protobuf_version} --pakdir=$route/../external_libs/debs >/dev/null 2>&1|| exit 4
 else
-    /usr/local/sbin/checkinstall -D --install=no --fstrans=no --default --pkgname=protobuf --pkgversion=${protobuf_version} --pakdir=$route/../../external_libs/rpms || exit 4
+    /usr/local/sbin/checkinstall -D --install=no --fstrans=no --default --pkgname=protobuf --pkgversion=${protobuf_version} --pakdir=$route/../external_libs/rpms >/dev/null 2>&1 || exit 4
 fi
 
-sudo rm -rf grpc/
+printf "${GREEN}========== gen grpc&& protobuf package success ===========\n"
+sudo rm -rf $route/grpc
+
